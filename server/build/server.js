67371cbbdae5ef6c67b5f24602c062e47651b197
Lindemberg Nunes de Castro <lindembergncastro@outlook.com>
2022-09-30 23:11:12 -0300

    feat: add typescript

---
+++ server/build/server.js
@@ -0,0 +1,10 @@
+import express from 'express';
+var app = express();
+app.get('/users', function (req, res) {
+    return res.json([
+        { id: 1, name: 'Anuncio 1' },
+        { id: 2, name: 'Anuncio 2' },
+        { id: 3, name: 'Anuncio 3' },
+    ]);
+});
+app.listen(3333);
